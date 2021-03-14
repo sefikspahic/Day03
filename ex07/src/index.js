4a30f14cdfa743c060ac475c0dfa592414b01be1
sefikspahice <spahicsefik@gmail.com>
2021-03-14 17:00:56 +0100

    fix ex07,ex11,ex12,ex13,ex14,ex15

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,24 +1,21 @@
 function main() {
-    // Only change code below this line
-    // Variable declarations
-    var studlyCapVar;
-    var properCamelCase;
-    var titleCaseOver;
-    // Variable assignments
-    studlyCapVar = 10
-    properCamelCase = "A String";
-    titleCaseOver = 9000;
+  // Only change code below this line
+  // Variable declarations
+  var studlyCapVar;
+  var properCamelCase;
+  var titleCaseOver;
+  // Variable assignments
+  studlyCapVar = 10;
+  properCamelCase = "A String";
+  titleCaseOver = 9000;
 
+  return {
+    studlyCapVar,
+    properCamelCase,
+    titleCaseOver,
+  };
 
-
-    return {
-        studlyCapVar,
-        properCamelCase,
-        titleCaseOver
-    }
-
-    // Only change code above this line
-
+  // Only change code above this line
 }
 console.log(main());
 module.exports = main;
